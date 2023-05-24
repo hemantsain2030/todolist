43b31653794f8534d1b2f3a16724c721119e31a2
Hemant Sain <hemantsain2030@gmail.com>
2023-05-24 10:59:28 +0530

    My first commit Reacttodolistwebsite

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import ToDoLists from "./ToDoLists";
+
+
+const App = () => {
+  const [inputList, setInputList] = useState("buy apple");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteItems = (id)=>{
+    console.log("Item is deleted");
+
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem, index)=>{
+        return index !== id;
+      });
+    })
+  }
+
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1> ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add a Item"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button onClick={listOfItems}> + </button>
+
+          <ol>
+            {/* <li>{inputList}</li> */}
+            {items.map((itemVal, index) => {
+              return <ToDoLists 
+              text={itemVal}
+              key={index}
+              id={index}
+              onSelect={deleteItems}
+              />
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
